0a3fbfdd2a89ef4e0bbc7a0cfa3eb920e6388d42
Akshu <akshushigwan2000@gmail.com>
2022-01-31 15:20:25 +0530

    Updates

---
+++ precache-manifest.221149a2bcf3e3148c516a2c6a625edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b1d438f64fb192c05a597cf73669f5",
+    "url": "/myapp2/index.html"
+  },
+  {
+    "revision": "35c0ba95265f66a4d764",
+    "url": "/myapp2/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "47e615976f7de7ec1bb4",
+    "url": "/myapp2/static/js/2.560c0739.chunk.js"
+  },
+  {
+    "revision": "10b61eb3fc61dfd2241d",
+    "url": "/myapp2/static/js/3.80c7ac3e.chunk.js"
+  },
+  {
+    "revision": "35c0ba95265f66a4d764",
+    "url": "/myapp2/static/js/main.4993f424.chunk.js"
+  },
+  {
+    "revision": "3da327d5de087e2ae025",
+    "url": "/myapp2/static/js/runtime~main.8c29d7ce.js"
+  }
+]);
